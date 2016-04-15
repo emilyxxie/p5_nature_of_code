6e6e13634134991fbb70c3cbdaaf4571053dc535
emilyxxie <emilyxxie@gmail.com>
2016-04-14 21:42:47 -0400

    make it blue.

--- 3_custom_distribution.js
+++ 3_custom_distribution.js
@@ -2,9 +2,13 @@ var lineX = 0;
 var lineWidth = 2;
 var randomThreshold = 1.15;
 var randomNumberTotal = 2;
+var bIncrement;
+var colorB = 0;
 
 function setup() {
   createCanvas(800, 800);
+  // total color divided by total strokes)
+  bIncrement = 255 / (width / lineWidth);
 }
 
 
@@ -13,12 +17,14 @@ function draw() {
   // draws line of randomly generated height that gets increasingly taller on average.
   // spare time: get the "dips" in the graph to get increasingly smaller
   while (!create) {
+    colorB += bIncrement;
     var randomNumber = random(randomNumberTotal);
     if (randomNumber < randomThreshold) {
       randomNumberTotal = randomNumberTotal * 1.025;
       randomThreshold = randomThreshold * randomThreshold;
       strokeWeight(lineWidth);
       lineX += lineWidth;
+      stroke(0, 0, colorB);
       line(lineX, height, lineX, height - randomNumber);
       create = true;
     }
