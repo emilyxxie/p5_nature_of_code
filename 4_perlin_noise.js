b87eb1fadd9fe9e900c0f699b5910071413d77cc
emilyxxie <emilyxxie@gmail.com>
2016-04-15 19:01:13 -0400

    bezier curve with perlin

--- 4_perlin_noise.js
+++ 4_perlin_noise.js
@@ -1,22 +1,22 @@
-var yNoise = 0;
-var mappedXWidth;
+var noiseFactor = 0;
 
 function setup() {
   createCanvas(screen.availWidth / 2, screen.availHeight /2 );
   mappedXWidth = width;
-
+  background(255);
 }
 
 function draw() {
-  background(0);
+  stroke(0);
   noFill();
-  stroke(255);
-  beginShape();
-  for (var x = 0; x < width; x++) {
-    mappedX = map(x, 0, mappedXWidth, 0, 2);
-    var y = height * noise(mappedX, yNoise);
-    vertex(x, y);
-  }
-  yNoise += 0.01;
-  endShape();
+  var x1 = width * noise(noiseFactor + 10);
+  var y1 = height * noise(noiseFactor + 20);
+  var x2 = width * noise(noiseFactor + 40);
+  var y2 = height * noise(noiseFactor + 60);
+  var x3 = width * noise(noiseFactor + 70);
+  var y3 = height * noise(noiseFactor + 80);
+  var x4 = width * noise(noiseFactor + 90);
+  var y4 = height * noise(noiseFactor + 100);
+  bezier(x1, y1, x2, y2, x3, y3, x4, y4);
+  noiseFactor += 0.0025;
 }
