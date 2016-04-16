ba4e46a1b8e4e1a1f3ad498b970c63e083d09b9c
emilyxxie <emilyxxie@gmail.com>
2016-04-15 21:39:11 -0400

    more perlin noise play

--- 5_perlin_black_flower.js
+++ 5_perlin_black_flower.js
@@ -4,8 +4,8 @@ var totalFrames = 0;
 function setup() {
   createCanvas(screen.availWidth, screen.availHeight);
   mappedXWidth = width;
-  background(255);
-  stroke(0, 20);
+  background(0);
+  stroke(255, 10);
   noFill();
 }
 
@@ -15,14 +15,17 @@ function draw() {
     background(255);
     totalFrames = 0;
   }
-  var x1 = width * noise(noiseFactor + width / 4),
+  var xAnchor = width / 2;
+
+  var x1 = width * noise(noiseFactor + width / 5),
       y1 = height * noise(noiseFactor + height / 4),
-      x2 = width * noise(noiseFactor + width / 3),
-      y2 = height * noise(noiseFactor + height / 3),
-      x3 = width * noise(noiseFactor + width / 2),
-      y3 = height * noise(noiseFactor + height / 2),
+      x2 = width * noise(noiseFactor + width / 2),
+      y2 = height * noise(noiseFactor + height / 2),
+      x3 = width * noise(noiseFactor + width / 0.1),
+      y3 = height * noise(noiseFactor + height / 0.1),
       x4 = width * noise(noiseFactor + width / 0.2),
       y4 = height * noise(noiseFactor + height / 0.2);
-  bezier(x1, y1, x2, y2, x3, y3, x4, y4);
-  noiseFactor += 0.002;
+  bezier(x1, y1, x2, y2, x3, y3, xAnchor, height);
+  noiseFactor += 0.003;
+
 }
