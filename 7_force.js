d673c60c9d44bcd21ff869f93a4dd8df4cf57522
emilyxxie <emilyxxie@gmail.com>
2016-04-18 16:57:35 -0400

    simulate falling balls

--- 7_force.js
+++ 7_force.js
@@ -1,36 +1,44 @@
-var ball;
+var balls = [];
 
 function setup() {
   createCanvas(screen.availWidth, screen.availHeight);
-  ball = new Ball();
+  for (var i = 0; i <= 5; i++) {
+    ball = new Ball();
+    balls.push(ball);
+  }
+  console.log(balls);
 }
 
 function draw() {
   background(255);
-  ball.update();
-  gravity = createVector(0, 0.3);
-  ball.applyForce(gravity);
-  wind = createVector(-0.2, 0);
-  if (mouseIsPressed) {
-    ball.applyForce(wind);
-  }
-  ball.render();
+  balls.forEach(function(ball) {
+    gravity = createVector(0, 0.3);
+    gravity.mult(ball.size);
+    ball.applyForce(gravity);
+    wind = createVector(-1, 0);
+    if (mouseIsPressed) {
+      ball.applyForce(wind);
+    }
+    ball.render();
+    ball.update();
+  });
+
 }
 
 function Ball() {
-  this.location = createVector(width / 2, height / 2);
+  this.location = createVector(random(width), height / 2);
   this.velocity = createVector();
   this.acceleration = createVector();
-  this.size = 60;
+  this.size = random(100);
 
   this.applyForce = function(force) {
+    var force = p5.Vector.div(force, this.size);
     this.acceleration.add(force);
   }
 
   this.update = function() {
     this.velocity.add(this.acceleration);
     this.location.add(this.velocity);
-    console.log(this.velocity);
     this.acceleration.mult(0);
     this.bounce();
   }
@@ -46,7 +54,7 @@ function Ball() {
   }
 
   this.render = function() {
-    fill(0);
+    fill(0, 40);
     ellipse(this.location.x, this.location.y, this.size, this.size);
   }
 }
