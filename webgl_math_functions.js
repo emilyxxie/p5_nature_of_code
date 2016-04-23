a5e1b35b5e9e8cc9b9908027c6eb394009094b4a
emilyxxie <emilyxxie@gmail.com>
2016-04-23 00:15:33 -0400

    playing with tan cos sin

--- webgl_math_functions.js
+++ webgl_math_functions.js
@@ -1,3 +1,5 @@
+var img;
+
 function setup(){
   createCanvas(
     window.innerWidth,
@@ -7,19 +9,20 @@ function setup(){
 }
 
 function draw(){
+
   background(0);
   for(var x = 0; x < 5; x++){
     rotateY(frameCount * 0.001);
     push();
     for(var i = 0; i < 60; i++){
       translate(
-        tan(frameCount * 0.01 + x) * 100,
-        tan(frameCount * 0.001 + x) * 20,
-        i * 0.0001
+        tan(frameCount * 0.002 + x) * 100,
+        tan(frameCount * 0.0001 + x) * 50,
+        sin(i * 0.001)
       );
       rotateZ(frameCount * 0.002);
       push();
-      torus(8);
+      sphere(13);
       pop();
     }
     pop();
